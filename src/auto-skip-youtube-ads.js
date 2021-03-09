09129699db39ffbeb95a79671097dd1d5b76221f
alhacen <alhaqhassan@gmail.com>
2021-03-09 22:00:27 +0530

    now it shows video speed inplace of youtube logo

--- src/auto-skip-youtube-ads.js
+++ src/auto-skip-youtube-ads.js
@@ -14,6 +14,6 @@ function __(a){return document.getElementsByClassName(a);}
 	},100)
 function speedController_(e){
     if(e.keyCode==107 || e.keyCode==187){document.getElementsByTagName("video")[0].playbackRate += .25;}else if(e.keyCode==109 || e.keyCode==189){document.getElementsByTagName("video")[0].playbackRate -= .25;}
-document.getElementById("logo-icon-container").innerHTML=document.getElementsByTagName("video")[0].playbackRate+" x"
+document.getElementById("logo-icon").innerHTML=document.getElementsByTagName("video")[0].playbackRate+" x"
 }
 document.addEventListener('keydown', speedController_);
