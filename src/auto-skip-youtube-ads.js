6ec6e057a7c3c3013f5bcdbd0289f54286d96206
alhaqhassan <alhaqhassan@gmail.com>
2022-02-13 22:47:21 +0530

    feat: added speed indicator at navbar,

--- src/auto-skip-youtube-ads.js
+++ src/auto-skip-youtube-ads.js
@@ -1,19 +1,31 @@
-function __(a){return document.getElementsByClassName(a);}
-	console.log("started");
-	setInterval(function(){
-		prev_playback_speed=document.getElementsByTagName("video")[0].playbackRate;
-	    if(__("ytp-ad-text").length!=0){//if ad
-	        document.getElementsByTagName("video")[0].playbackRate = 16;//play video on 16x speed
-	    }else{
-	        document.getElementsByTagName("video")[0].playbackRate = prev_playback_speed;//play video on normal speed
-	    }
-	    if(__("ytp-ad-skip-button-text")[0]!=undefined){//if skip button is enabled
-	        __("ytp-ad-skip-button-text")[0].click();
-	    }else{
-	    }
-	},100)
-function speedController_(e){
-    if(e.keyCode==107 || e.keyCode==187){document.getElementsByTagName("video")[0].playbackRate += .25;}else if(e.keyCode==109 || e.keyCode==189){document.getElementsByTagName("video")[0].playbackRate -= .25;}
-document.getElementById("logo-icon").innerHTML=document.getElementsByTagName("video")[0].playbackRate+" x"
+function __(a) {
+  return document.getElementsByClassName(a);
 }
-document.addEventListener('keydown', speedController_);
+document.getElementById("logo-icon").innerHTML = document.getElementsByTagName("video")[0].playbackRate + " x";
+document.getElementById("logo-icon").removeAttribute('hidden')
+let playbackRate = document.getElementsByTagName("video")[0].playbackRate;
+if (!window.skipAdInterval) {
+	console.log("started");
+  //if already activated
+	window.skipAdInterval = setInterval(function () {
+		if (__("ytp-ad-text").length != 0) { //if ad
+			document.getElementsByTagName("video")[0].playbackRate = 16; //play video on 16x speed
+		} else {
+			document.getElementsByTagName("video")[0].playbackRate = playbackRate; //play video on normal speed
+		}
+		if (__("ytp-ad-skip-button-text")[0] != undefined) { //if skip button is enabled
+			__("ytp-ad-skip-button-text")[0].click();
+		}
+	}, 100);
+	function speedController_(e) {
+	  if (e.keyCode == 107 || e.keyCode == 187) {
+		document.getElementsByTagName("video")[0].playbackRate += 0.25;
+	  } else if (e.keyCode == 109 || e.keyCode == 189) {
+		document.getElementsByTagName("video")[0].playbackRate -= 0.25;
+	  }
+	  playbackRate = document.getElementsByTagName("video")[0].playbackRate
+	  document.getElementById("logo-icon").innerHTML = document.getElementsByTagName("video")[0].playbackRate + " x";
+	}
+	document.addEventListener("keydown", speedController_);
+}
+
